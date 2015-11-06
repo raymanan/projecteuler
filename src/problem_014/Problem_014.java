58b7822c3870deec4c7538a93e70213c03771b3f
raymanan <raymanan@163.com>
2015-11-06 11:24:03 +0800

    dos2unix

--- src/problem_014/Problem_014.java
+++ src/problem_014/Problem_014.java
@@ -5,44 +5,43 @@ import java.util.List;
 
 public class Problem_014 {
   public static void main(String[] args) {
-	long beginTime = System.currentTimeMillis();
+    long beginTime = System.currentTimeMillis();
 
-	System.out.println(longestCollatzSequence(1000000));
+    System.out.println(longestCollatzSequence(1000000));
 
-	System.out.println("interval: " + (System.currentTimeMillis() - beginTime)
-	                   / 1000 + "s");
+    System.out.println("interval: " + (System.currentTimeMillis() - beginTime) / 1000 + "s");
   }
 
   private static long longestCollatzSequence(long count) {
-	long longestChain = 0;
-	long start = 0;
+    long longestChain = 0;
+    long start = 0;
 
-	for (int i = 0; i <= count; i++) {
-	  int length = getCollatzSequence(i).length;
-	  // System.out.println("startNumber: " + i + "; length: " + length);
-	  if (length > longestChain) {
-		longestChain = length;
-		start = i;
-	  }
-	}
-	return start;
+    for (int i = 0; i <= count; i++) {
+      int length = getCollatzSequence(i).length;
+      // System.out.println("startNumber: " + i + "; length: " + length);
+      if (length > longestChain) {
+        longestChain = length;
+        start = i;
+      }
+    }
+    return start;
   }
 
   private static Long[] getCollatzSequence(long startNumber) {
-	List<Long> seq = new ArrayList<Long>();
+    List<Long> seq = new ArrayList<Long>();
 
-	long n = startNumber;
-	seq.add(n);
-	while (n > 1) {
-	  if (n % 2 == 0) {
-		n = n / 2;
-	  } else {
-		n = 3 * n + 1;
-	  }
+    long n = startNumber;
+    seq.add(n);
+    while (n > 1) {
+      if (n % 2 == 0) {
+        n = n / 2;
+      } else {
+        n = 3 * n + 1;
+      }
 
-	  seq.add(n);
-	}
+      seq.add(n);
+    }
 
-	return (Long[]) seq.toArray(new Long[seq.size()]);
+    return (Long[]) seq.toArray(new Long[seq.size()]);
   }
 }
--- src/problem_015/Problem_015.java
+++ src/problem_015/Problem_015.java
@@ -5,36 +5,35 @@ import java.util.List;
 
 public class Problem_015 {
   public static void main(String[] args) {
-	long beginTime = System.currentTimeMillis();
+    long beginTime = System.currentTimeMillis();
 
-	System.out.println(latticePaths(20));
+    System.out.println(latticePaths(20));
 
-	System.out.println("interval: " + (System.currentTimeMillis() - beginTime)
-	                   / 1000 + "s");
+    System.out.println("interval: " + (System.currentTimeMillis() - beginTime) / 1000 + "s");
   }
 
   private static long latticePaths(int count) {
-	long routes = 0;
-	Grid grid = generateGrid(count);
-	List<Point> points = grid.getPoints();
-	for (int i = 0; i < points.size(); i++) {
+    long routes = 0;
+    Grid grid = generateGrid(count);
+    List<Point> points = grid.getPoints();
+    for (int i = 0; i < points.size(); i++) {
 
-	}
-	return 0;
+    }
+    return 0;
   }
 
   private static Grid generateGrid(int count) {
-	Grid grid = new Grid(count);
-	for (int i = 0; i < count; i++) {
-	  for (int j = 0; j < count; j++) {
-		Point point = new Point(i, j);
-		grid.addPoint(point);
-	  }
-	}
+    Grid grid = new Grid(count);
+    for (int i = 0; i < count; i++) {
+      for (int j = 0; j < count; j++) {
+        Point point = new Point(i, j);
+        grid.addPoint(point);
+      }
+    }
 
-	return grid;
+    return grid;
   }
-  
+
   private static Tree generateTree(int count){
 	List<Node> nodes=new ArrayList<Node>();
 	Tree tree=new Tree();
